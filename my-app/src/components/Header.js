75052b6d4fe0e698bfa2a2ad9eeea360ccabed2f
Nhat Khang Alan To <2289642@collegemv.qc.ca>
2025-02-24 17:47:15 -0500

    notification with backend

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,16 +1,19 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import logo from '../image/logo.png';
 
 function Header() {
     const navigate = useNavigate();
     const [activePage, setActivePage] = useState(window.location.pathname);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [showNotifications, setShowNotifications] = useState(false);
+    const [notifications, setNotifications] = useState([]);
+    const [unreadCount, setUnreadCount] = useState(0);
 
     useEffect(() => {
         const checkToken = () => {
             const token = localStorage.getItem('authToken');
-
             if (token) {
                 setIsAuthenticated(true);
             } else {
@@ -21,6 +24,35 @@ function Header() {
         checkToken();
     }, []);
 
+    useEffect(() => {
+        if (isAuthenticated) {
+            const fetchNotifications = async () => {
+                try {
+                    const response = await axios.get('https://localhost:3001/api/notifications', {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+                        },
+                    });
+
+                    console.log(response.data);
+                    setNotifications(response.data);
+
+                    let unreadCount = 0;
+                    for (const notification of response.data) {
+                        if (!notification.isRead) {
+                            unreadCount++;
+                        }
+                    }
+                    setUnreadCount(unreadCount);
+                } catch (error) {
+                    console.error('Error fetching notifications:', error);
+                }
+            };
+
+            fetchNotifications();
+        }
+    }, [isAuthenticated]);
+
     const handleNavLinkClick = (path) => {
         setActivePage(path);
     };
@@ -29,6 +61,40 @@ function Header() {
         setIsAuthenticated(false);
         localStorage.removeItem('authToken');
         navigate('/signIn');
+    };
+
+    const toggleNotifications = (e) => {
+        e.preventDefault();
+        setShowNotifications(!showNotifications);
+    };
+
+    const deleteNotification = async (notificationId) => {
+        try {
+            await axios.delete(`https://localhost:3001/api/notifications/${notificationId}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+                },
+            });
+
+            setNotifications(prevNotifications =>
+                prevNotifications.filter(notification => notification._id !== notificationId)
+            );
+
+            let unreadCount = 0;
+            for (const notification of notifications) {
+                if (!notification.isRead) {
+                    unreadCount++;
+                }
+            }
+            setUnreadCount(unreadCount);
+        } catch (error) {
+            console.error('Error deleting notification:', error);
+        }
+    };
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
     };
 
     return (
@@ -56,7 +122,45 @@ function Header() {
                             {isAuthenticated ? (
                                 <>
                                     <li className="nav-item">
-                                        <a className={`nav-link ${activePage === '/notifications' ? 'active' : ''}`} href="/notifications" onClick={() => handleNavLinkClick('/notifications')}>Notifications</a>
+                                        <a
+                                            className={`nav-link ${activePage === '/notifications' ? 'active' : ''}`}
+                                            href="/"
+                                            onClick={(e) => toggleNotifications(e)}
+                                        >
+                                            <i className={`bi ${unreadCount > 0 ? 'bi-bell-fill' : 'bi-bell'}`}></i>
+
+                                            {unreadCount > 0 && (
+                                                <span className="badge bg-danger ms-1">{unreadCount}</span>
+                                            )}
+                                        </a>
+                                        {showNotifications && (
+                                            <div className="dropdown-menu show" style={{ position: 'absolute', top: '60px', right: '10px', width: '300px', zIndex: '1000', padding: '10px', borderRadius: '10px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
+                                                <ul className="list-group">
+                                                    {notifications.map((notification) => (
+                                                        <li key={notification.notifId} className={`list-group-item position-relative ${notification.isRead ? '' : 'bg-light'} mb-3 p-3 rounded-3`}>
+                                                            <div className="d-flex justify-content-between align-items-start">
+                                                                <span>{notification.message}</span>
+                                                            </div>
+                                                            {!notification.isRead && (
+                                                                <div className="mt-2">
+                                                                    <span className="badge bg-warning">Unread</span>
+                                                                </div>
+                                                            )}
+                                                            <div className="mt-2 text-muted" style={{ fontSize: '0.8rem' }}>
+                                                                {formatDate(notification.date)}
+                                                            </div>
+                                                            <button
+                                                                className="btn btn-sm btn-danger position-absolute"
+                                                                style={{ bottom: '10px', right: '10px' }}
+                                                                onClick={() => deleteNotification(notification._id)}
+                                                            >
+                                                                X
+                                                            </button>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        )}
                                     </li>
                                     <li className="nav-item">
                                         <a className={`nav-link ${activePage === '/settings' ? 'active' : ''}`} href="/settings" onClick={() => handleNavLinkClick('/settings')}>Settings</a>
